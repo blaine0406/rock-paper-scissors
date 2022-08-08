da325f30623ec3bdc48eb1be08f9717b7a3faf33
Chris <blaine0406@gmail.com>
2022-08-08 07:47:37 -0400

    added playRound function

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
-
+//This is from scratch
 
 function getComputerChoice() {
     let randomNumber = Math.floor( Math.random()*3 ) + 1
     if (randomNumber === 1) {
-        console.log("Rock");
+        return("Rock");
     } else if (randomNumber === 2) {
-        console.log("Paper");
+        return("Paper");
     } else {
-        console.log("Scissors");
+        return("Scissor");
+    }
+   
+}
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "Rock") {
+        return "Draw"
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return "You Lose. Paper beats Rock!"
+    } else if (playerSelection === "Rock" && computerSelection === "Scissor") {
+        return "You Win. Rock beats Scissor!"
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You win! Paper beats Rock!"
+    } else if (playerSelection === "Paper" && computerSelection === "Scissor") {
+        return "You lose. Scissor beats Paper!"
+    } else if (playerSelection === "Scissor" && computerSelection === "Rock") {
+        return "You lose. Rock beats Scissor"
+    } else if (playerSelection === "Scissor" && computerSelection === "Paper") {
+        return "You win. Scissor beats Paper!"
+    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+        return "Draw"
+    } else {
+        return "Draw"
     }
 }
 
-getComputerChoice()
+const playerSelection = "Rock"
+const computerSelection = getComputerChoice()
 
-
-//let randomNumber = Math.floor( Math.random()*3 ) + 1
-//console.log(randomNumber);
+console.log(playRound());
